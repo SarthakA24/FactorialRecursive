1009d99c0d5a194b0759e06de33e2bd9a965c1de
dbc2201 <divyanshb22@gmail.com>
2022-07-11 00:35:00 +0530

    complete the problem

---
+++ src/main/java/io/github/dbc/RecursiveFactorialCalculator.java
@@ -0,0 +1,14 @@
+package io.github.dbc;
+
+public class RecursiveFactorialCalculator {
+    /**
+     * This method calculates the factorial of a number.
+     * The method returns 0 for a negative number.
+     *
+     * @param number the number to calculate the factorial of.
+     * @return the factorial of the given number.
+     */
+    public static int factorial(int number) {
+        return 0;
+    }
+}
